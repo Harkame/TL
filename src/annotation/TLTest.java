57c8e8f021878750b66a054b5e98440c37cc3df9
harkame <louisr.daviaud@gmail.com>
2016-12-05 16:00:56 +0100

    clean

--- src/annotation/TLTest.java
+++
@@ -1,8 +0,0 @@
-package annotation;
-
-public @interface TLTest
-{
-	public enum STATUS_TEST {NOTEST, TOTEST, TESTED};
-
-	public abstract STATUS_TEST status(); 
-}
--- src/model/tache/liste_tache/ListeTacheComplex.java
+++
@@ -1,6 +0,0 @@
-package model.tache.liste_tache;
-
-public class ListeTacheComplex
-{
-
-}
